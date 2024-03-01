83f47e4d976540064ea4975f07a9e51b31db1fed
danieltrizotto <danielbaltri@gmail.com>
2024-02-29 22:04:56 -0300

    alteraçoes mobile e js login

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 function logar() {
-    var nome = document.getElementById("nome").value;
-    var senha = document.getElementById("senha").value;
+    var nome = document.getElementById("nome").value.trim();
+    var senha = document.getElementById("senha").value.trim();
+    var enviar = document.getElementById("entrar");
 
-    if (nome == "daniel" && senha == "12345") {
-        alert("acesso garantido");
-        location.href = ("./pages/teste.html");
+    if (nome === "daniel" && senha === "12345") {
+        alert("Acesso garantido");
+        location.href = "./pages/teste.html";
     } else {
-        alert("acesso negado");
+        alert("Acesso negado");
+    }
+
+
+    if (nome === "" || senha === "") {
+        alert("Preencha as informações");
     }
 
 
