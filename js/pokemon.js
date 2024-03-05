2e3adf675c41e346cc00fa0f65f531fd7356be65
danieltrizotto <danielbaltri@gmail.com>
2024-03-05 14:50:22 -0300

    fetch e responsividade

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,8 +1,14 @@
-const btnBusca = document.getElementById("btnBuscar");
+
+
+btnBusca.addEventListener("click",function(){
+  const btnBusca = document.getElementById("btnBuscar");
 const inputBusca = document.getElementById("txtBusca").value.trim();
 
-addEventListener.btnBusca("click",function(){
 const url = "https://pokeapi.co/api/v2/pokemon/";
+const ul = document.getElementById('pokemonPesquisado');
+const list = document.createDocumentFragment();
+
+
 fetch(url + inputBusca)
 .then(response =>{
 if(!response.ok){
@@ -11,13 +17,19 @@ if(!response.ok){
 return response.json();
 })
 .then(data => {
-    // Faça algo com os dados retornados pela API
-    console.log(data);
+console.log(data);
+   /* let pokemon = data;
+   pokemon.map(function(info) {
+      let li = document.createElement('li');
+      let nome = document.createElement('h2');
+      let abilidade = document.createElement('span');
+
+      nome.innerHTML = `${info.nome}`;
+     abilidade.innerHTML = `${info.abilidade}`;
+
+      li.appendChild(nome);
+      li.appendChild(abilidade);
+      list.appendChild(li); */
   })
-  .catch(error => {
-    console.error('Houve um problema com a sua requisição fetch:', error);
-  });
+})
 
-}
-
-)
